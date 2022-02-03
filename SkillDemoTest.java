9d69ea1cede34a60aaa9c3c0275d018a81c11190
pelmini <37095187+pelmini@users.noreply.github.com>
2022-02-02 16:09:39 -0800

    Fixed test that was failing

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void multiply() {
-        assertEquals(2, SkillDemo.multiply(1, 5));
+        assertEquals(2, SkillDemo.multiply(1, 2));
     }
 }
